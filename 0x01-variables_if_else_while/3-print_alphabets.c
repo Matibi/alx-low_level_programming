3ceaff8f50e486c528a3d5e9fed87163eed0195c
Matibi <49315201+Matibi@users.noreply.github.com>
2022-12-09 23:13:41 +0200

    Betty Style

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,12 @@ int main(void)
 	char c, d;
 
 	for (c = 'a'; c <= 'z'; c++)
-	{
 		putchar(c);
-	}
+		
 	for (d = 'A'; d <= 'Z'; d++)
-	{
 		putchar(d);
-	}
+	
 	putchar(10);
+	
 	return (0);
 }
