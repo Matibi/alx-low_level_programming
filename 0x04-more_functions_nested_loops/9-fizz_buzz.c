a66827424e39fdb161af767425a0c4602c76b72d
Matibi <matibi@live.com>
2022-12-15 14:49:21 +0200

    Semantic error updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,33 +8,33 @@
 
 int main(void)
 {
-    int i = 1;
+int i = 1;
 
-    while (i <= 100)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-        {
-            printf("FizzBuzz");
-        }
-        else if (i % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else
-        {
-            printf("%i", i);
-        }
-        if (i != 100)
-        {
-            putchar(' ');
-        }
+while (i <= 100)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%i", i);
+}
+if (i != 100)
+{
+putchar(' ');
+}
 
-        i++;
-    }
-    putchar('\n');
-    return (0);
+i++;
+}
+putchar('\n');
+return (0);
 }
